36a9839c7c8b3351e130b9d64dfdaf6a56f57185
ChugaevMV <chugaevmv@gmail.com>
2023-01-29 17:45:52 +0500

    Update main.cpp

--- devicemonitoringserver/main.cpp
+++ devicemonitoringserver/main.cpp
@@ -2,6 +2,19 @@
 #include "tests.h"
 #include <servermock/servertests.h>
 
+#include <devicemock.h>
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include <MessageSerializer.h>
+#include <ro3.h>
+#include <mirror.h>
+#include <multiply41.h>
+#include <messageencoder.h>
+
+using namespace std;
 int main()
 {
     TestRunner tr;
@@ -9,10 +22,86 @@ int main()
     RUN_TEST(tr, safeObjectPointerTest);
     RUN_TEST(tr, connectionChannelTest);
     RUN_TEST(tr, clientServerTest);
+    RUN_TEST(tr, monitoringServerTest1);
 
-    std::cout << "Hello" << std::endl;
+        //Проверка работы MessageSerializer
+    string Type1 = {"Meterage"}, Type2;
+    uint8_t Meterage1 = 50;
+    uint8_t Meterage2;
+    uint64_t Time1 = 5, Time2;
+    double Command = -7.5;
+    string Type_error = {"NoTimestamp"};
+
+    MessageSerialiser Message;
+
+
+    string Write1 = Message.serialize_Message(Meterage1,Time1);
+    string Write2 = Message.serialize_Message(Command);
+    string Write3 = Message.serialize_Message(Type_error);
+
+    cout << Write1 << endl;
+//    cout << Write2 << endl;
+//    cout << Write3 << endl;
+
+/*
+    cout << Message.GetTypeMessage(Write1) << endl;
+    cout << Message.GetMeterage(Write1) << endl;
+    cout << Message.GetTimeStamp(Write1) << endl;
+    cout << Message.GetTypeError(Write3) << endl;
+    cout << Message.GetCommand(Write2) << endl;
+*/
+
+
+
+//   RO3 ro3;
+
+//   RO3 *p_ro3 = &ro3;
+
+//   BaseEncoderExecutor *p_U = reinterpret_cast <BaseEncoderExecutor*> (p_ro3);
+
+//   MessageEncoder Encode;
+
+//   Encode.registration_algorithm(p_U);
+
+//   string M = Encode.encode(Write1);
+
+//   string N = Encode.decode(M);
+
+//   cout << "M= " << M << "     " << "N= " << N << endl;
+
+
+   //Multiply41 multiply;
+
+   //Multiply41 *p_multiply = &multiply;
+
+   //BaseEncoderExecutor *p_U1 = reinterpret_cast <BaseEncoderExecutor*> (p_multiply);
+
+//   MessageEncoder Encode;
+
+//   Multiply41 multiply;
+
+//   RO3 ro3;
+
+//   Mirror mirror;
+
+//   BaseEncoderExecutor *p_U1 = reinterpret_cast <BaseEncoderExecutor*> (&multiply);
+
+//   //Encode.choice_algoithm(P1);
+
+//   Encode.registration_algorithm(p_U1);
+
+//   string M1 = Encode.encode(Write1);
+
+//   //cout << "M = " << M1 << endl;
+
+//   string N1 = Encode.decode(M1);
+
+//   //cout << "N = " << N1 << endl;
+
+//   cout << "M = " << M1 << " " << "N = " << N1 << endl;
+
 
     // TODO: собственные тесты
-    RUN_TEST(tr, monitoringServerTest1);
+
     return 0;
 }
