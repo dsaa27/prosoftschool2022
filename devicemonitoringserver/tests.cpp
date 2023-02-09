2de549a7115f14b2599b1961ce5b1ad939d8bee5
ChugaevMV <chugaevmv@gmail.com>
2023-02-10 00:17:30 +0500

    Update tests.cpp

--- devicemonitoringserver/tests.cpp
+++ devicemonitoringserver/tests.cpp
@@ -15,23 +15,41 @@ void monitoringServerTest1()
     DeviceMock device(new ClientConnectionMock(taskQueue));
     DeviceMonitoringServer server(new ConnectionServerMock(taskQueue));
 
-    const uint64_t deviceId = 111;
-    const uint64_t serverId = 11;
-
-    std::vector<uint8_t> meterages {1};
-    std::vector<Phase> phases {{0,1}};
-    DeviceWorkSchedule schedule {deviceId,phases};
-
-
-
+    const uint64_t deviceId = 1;
+    const uint64_t serverId = 13;
 
     ASSERT(device.bind(deviceId));
     ASSERT(server.listen(serverId));
     ASSERT(device.connectToServer(serverId));
-    // device.setMeterages(  );
-    // device.startMeterageSending();
+
+    std::vector<uint8_t> meterages {32,5,150};
+    std::vector<Phase> phases {{0,1},{1,5},{2,250}};
+    DeviceWorkSchedule schedule {deviceId,phases};
+
+    server.setDeviceWorkSchedule(schedule);
+
+    device.setMeterages(meterages);
+
+    RO3 ro3;
+    BaseEncoderExecutor* p_ro3 = reinterpret_cast <BaseEncoderExecutor*> (&ro3);
+
+    device.setEncodingAlgoritm(p_ro3);
+    server.setEncodingAlgoritm(p_ro3);
+
     while (taskQueue.processTask())
         ;
 
+    device.startMeterageSending();
+
+    for (int i = 0; i < meterages.size()*2 + 2; i++)
+            taskQueue.processTask();
+
+
+
+    //ASSERT_EQUAL ()
+
+
+
     // ASSERT_EQUAL(device.responces(), expected);
 }
+
