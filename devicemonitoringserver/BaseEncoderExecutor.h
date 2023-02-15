07e1510fb518580d8f2236316c9eb3a4a59b25f9
ChugaevMV <chugaevmv@gmail.com>
2023-02-15 19:17:34 +0500

    Update BaseEncoderExecutor.h

--- devicemonitoringserver/BaseEncoderExecutor.h
+++ devicemonitoringserver/BaseEncoderExecutor.h
@@ -10,22 +10,21 @@
 class BaseEncoderExecutor
 {
 public:
-//    virtual ~BaseEncoderExecutor() = default;
 
     /*!
      * \brief Обработчик сообщения, которое нужно закодировать.
      */
-    virtual std::string encode(std::string &message);
+    virtual std::string encode(std::string &message) = 0;
 
     /*!
      * \brief Обработчик сообщения, которое нужно раскодировать.
      */
-    virtual std::string decode(std::string &message);
+    virtual std::string decode(std::string &message) = 0;
 
     /*!
      * \brief Возврат названия использованного алгоритма шифрования.
      */
-    virtual std::string name_of_encode_algorithm();
+    virtual std::string name_of_encode_algorithm() = 0;
 
 };
 #endif // BASEENCODEREXECUTOR_H
