ca030279cc6340bd0bdf215ae7180b3ada00f8e4
ChugaevMV <chugaevmv@gmail.com>
2023-02-15 19:18:27 +0500

    Update MessageSerializer.h

--- devicemonitoringserver/MessageSerializer.h
+++ devicemonitoringserver/MessageSerializer.h
@@ -6,84 +6,73 @@
 #include <string>
 #include <vector>
 
-//class AbstractMessageHandler;
 
 /*!
  * \brief Класс, создающий сообщения
  */
 class MessageSerialiser
 {
-//    NON_COPYABLE(MessageSerialiser)
-public:
-    /*!
-     * \brief Конструктор.
-     * \param Message - владеющий указатель на объект класса сериализатора сообщения
-     */
-//    MessageSerialiser(AbstractMessageHandler* Message);
-//    MessageSerialiser() = default;
-//    ~MessageSerialiser();
-
 
 public:
     /*!
      * \brief Создать сообщение со значением переменной
+     * \param meterages - значение передаваемого измеряемого параметра
+     * \param timeStamp - значение метки времени передаваемого измеряемого параметра
      */
-    //std::string do_serialize_Meterage_Message(const uint8_t &meterages, const uint64_t &timeStamp);
-
     std::string serialize_Message(const uint8_t &meterages, const uint64_t &timeStamp);
 
     /*!
      * \brief Создать командное сообщение
+     * \param command - значение поправочной команды
      */
-    //void do_serialize_Command_Message(double command);
     std::string serialize_Message(const double &command);
 
 
     /*!
      * \brief Создать сообщение об ошибке
+     * \param type_error - название передаваемой ошибки
      */
-    //void do_serialize_Error_Message(std::string& message);
     std::string serialize_Message(const std::string &type_error);
 
 
     /*!
-     * \brief Распарсить сообщение
+     * \brief Распарсить сообщение (функция для отладки)
+     * \param message - сообщение, которое нужно распарсить
      */
-    //void do_deserialize_Message(const std::string& message);
     void deserialize_Message(const std::string &message);
 
     /*!
      * \brief Вернуть тип передаваемого сообщения (строка)
+     * \param message - сообщение, которое нужно распарсить
      */
     std::string GetTypeMessage(const std::string &message);
 
     /*!
      * \brief Вернуть значение измерения
+     * \param message - сообщение, которое нужно распарсить
      */
     uint8_t GetMeterage(const std::string &message);
 
     /*!
      * \brief Вернуть метку времени
+     * \param message - сообщение, которое нужно распарсить
      */
     uint64_t GetTimeStamp(const std::string &message);
 
     /*!
      * \brief Вернуть тип ошибки
+     * \param message - сообщение, которое нужно распарсить
      */
     std::string GetTypeError(const std::string &message);
 
     /*!
      * \brief Вернуть значение команды
+     * \param message - сообщение, которое нужно распарсить
      */
     double GetCommand(const std::string &message);
 
 private:
-//    AbstractMessageHandler* m_Message = nullptr;
     std::string writtenMessage;
-    //std::string s_Type;
-    //uint64_t s_Time;
-    //uint8_t s_Meterage;
-    //std::string s_Type_error;
 };
 
 
