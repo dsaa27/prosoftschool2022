81d15c33c3d85d1be903185669b83a3a9be9f022
ChugaevMV <chugaevmv@gmail.com>
2023-02-15 19:18:45 +0500

    Update tests.h

--- devicemonitoringserver/tests.h
+++ devicemonitoringserver/tests.h
@@ -1,10 +1,24 @@
 #ifndef TESTS_H
 #define TESTS_H
 
+#include <vector>
+#include <iostream>
+#include "deviceworkschedule.h"
+
 // TODO: собственные тесты
 /*!
  * \brief Тест сервера.
  */
 void monitoringServerTest1();
+
+void serialiserTest();
+
+void encodeTestRO3();
+
+void encodeTestMirror();
+
+void encodeTestMultiply();
+
+std::vector <double> generateExpectedHistory(std::vector <uint8_t>& meterage,std::vector<Phase> shedule);
 
 #endif // TESTS_H
