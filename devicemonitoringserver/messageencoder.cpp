0204daaa35b6153b6289e08bb2fdd2a8a7d6e0e6
ChugaevMV <chugaevmv@gmail.com>
2023-02-15 19:18:12 +0500

    Update messageencoder.cpp

--- devicemonitoringserver/messageencoder.cpp
+++ devicemonitoringserver/messageencoder.cpp
@@ -20,28 +20,18 @@ void MessageEncoder::choice_algoithm(std::string &message)
 {
     if(message == "Multiply41")
     {
-        //if(v_NewEncodeAlgoritm != nullptr) delete v_NewEncodeAlgoritm;
-
-        //Multiply41* multiply = new Multiply41;
         Multiply41 multiply;
         v_NewEncodeAlgoritm = reinterpret_cast <BaseEncoderExecutor*> (&multiply);
-
     }
 
     if(message == "RO3")
     {
-        //if(v_NewEncodeAlgoritm != nullptr) delete v_NewEncodeAlgoritm;
-
-        //RO3* ro3 = new RO3;
         RO3 ro3;
         v_NewEncodeAlgoritm = reinterpret_cast <BaseEncoderExecutor*> (&ro3);
     }
 
     if(message == "Mirror")
     {
-        //if(v_NewEncodeAlgoritm != nullptr) delete v_NewEncodeAlgoritm;
-
-        //Mirror* mirror = new Mirror;
         Mirror mirror;
         v_NewEncodeAlgoritm = reinterpret_cast <BaseEncoderExecutor*> (&mirror);
     }
